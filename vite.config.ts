28e28dabb6815f075b3ab703f9c4e512ad409209
irenaldojs <irenaldojs@gmail.com>
2024-04-17 22:40:56 -0300

    config VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "WhatsApp Direto",
+        short_name: "WAP",
+        description: "WhatsApp Direto",
+        theme_color: "#034d3c",
+        background_color: "#034d3c",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
